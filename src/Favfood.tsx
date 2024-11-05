4d67a81f77939820ccfda5dec9339f4af196ef43
Justin Davidson <76005901+JustinDavidsonStruxure@users.noreply.github.com>
2024-11-05 15:26:46 -0600

    Refactor App component and add new components
    - Refactor App component to remove unused code and import statements
    - Add new components: FavFood, GreenCircle, and Greeting
    - Update App component to render the new components

---
+++ src/Favfood.tsx
@@ -0,0 +1,16 @@
+function FavFood() {
+    return (
+        <div style={{ fontFamily: 'monospace', whiteSpace: 'pre' }}>
+            {"   _____\n"}
+            {" /       \\\n"}
+            {"|         |\n"}
+            {"|   ___   |\n"}
+            {"|  /   \\  |\n"}
+            {"|  \\___/  |\n"}
+            {"|         |\n"}
+            {" \\_______/\n"}
+        </div>
+    );
+}
+
+export { FavFood };
